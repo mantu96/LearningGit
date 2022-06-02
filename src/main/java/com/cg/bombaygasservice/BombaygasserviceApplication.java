cc61745d49bccbebaf9d78be405d2091680125d8
mantu <mantuvishwakarma696@gmail.com>
2022-06-03 00:24:31 +0530

    this is first commit

---
+++ src/main/java/com/cg/bombaygasservice/BombaygasserviceApplication.java
@@ -0,0 +1,13 @@
+package com.cg.bombaygasservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BombaygasserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BombaygasserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cg/bombaygasservice/BombaygasserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.bombaygasservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BombaygasserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
